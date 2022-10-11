0b8c886634b40fcf8ccc798dd142f32f8239c8bb
philbert ndizeye <ndzphilbert@gmail.com>
2022-10-11 11:45:46 +0200

    fix alk

--- polls/urls.py
+++ polls/urls.py
@@ -13,4 +13,4 @@ urlpatterns =[
     path('student', views.student, name='student'),
     path('ussd/', views.ussdapp, name='ussd'),
     path('idaussd', views.idaussd, name='idauss')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
